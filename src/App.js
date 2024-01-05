a1d84d95db892e9c4be8a6809b80db4cc018c6b1
Monir EL OUARROUDI <info.elouarroudi@gmail.com>
2024-01-05 18:08:49 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Authenticate from './components/Authenticate';
 import RegisterComponent from './components/RegisterComponent';
+import GetALLTransfert from './components/GetALLTransfert';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Authenticate />} />
+        <Route path="/" element={<GetALLTransfert />} />
         <Route path="/register" element={<RegisterComponent />} />
         {/* Add more routes as needed */}
       </Routes>
